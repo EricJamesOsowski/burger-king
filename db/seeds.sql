3e8b7dad576c7e146bc3b615ab3b56419f8e2d69
Eric Osowski <ericosowski@Erics-MacBook-Pro.local>
2020-10-28 22:53:50 -0500

    initial file structuring

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, sleepy) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Triple Bacon Cheeseburger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Quarter Pound Ultra Delux', true);
+INSERT INTO burgers (name) VALUES ('Burger with a pickle');
